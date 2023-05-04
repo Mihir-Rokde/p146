efa5e324b348717b6ceda48aa331070fb6c6ad02
Mihir-Rokde <100846891+Mihir-Rokde@users.noreply.github.com>
2023-05-04 15:50:36 +0530

    Add files via upload

---
+++ P146.py
@@ -0,0 +1,30 @@
+from tkinter import*
+root=Tk()
+root.title("Fibonacci")
+root.geometry("400x400")
+
+textbox=Entry(root)
+label=Label(root,text="Fibonacci series : ")
+label2=Label(root,text="fibonacci sum : ")
+def Fibonacci():
+    input_no=int(textbox.get())
+    first_no=0
+    second_no=1
+    sum=0
+    sum2=0
+    counter=1
+    
+    while(counter<=input_no):
+        label["text"]+=str(sum)+" "
+        label2["text"]="Fibonacci sum : "+str(sum2)
+        counter=counter+1
+        first_no=second_no
+        second_no=sum
+        sum=first_no+second_no
+        sum2=sum2+sum
+textbox.pack()
+btn=Button(root,text="show fibonacci series",command=Fibonacci,fg="white",bg="dark blue")
+btn.pack()
+label.pack()
+label2.pack()
+root.mainloop()
